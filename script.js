75c81fc91be488d1a4a5aeef6a0e6fbf9eae6dea
Natalie Long <nflong1018@gmail.com>
2022-03-28 17:02:53 -0400

    updating form validation/displaying input

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ function myFunction() {
     }
   }
 
-  function showName(){
-    var message = document.getElementById("message").value;
-    document.getElementById("show").innerText = message
+  function display() {
+    var x=document.ideaForm.name.value;
+    var y=document.ideaForm.email.value;
+    var z=document.ideaForm.message.value;
+    alert("Thanks for reaching out! Here's what you've sent me. "+  "Your Name:"+x+"     "+"Your Email:"+y+"     "+"Your Message:"+z);
+  }
+
+function validateForm() {
+  let x = document.forms["ideaForm"]["email"].value;
+  if (x == "") {
+    alert("Oops! Please fill out your email address.");
+    return false;
+  }
 }
